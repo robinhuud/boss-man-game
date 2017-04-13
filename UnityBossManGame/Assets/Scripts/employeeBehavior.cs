796e8a7173b49d7cfb0097044517b3d35c9bf85c
Robinhuud <robinh@pobox.com>
2017-04-12 20:48:23 -0700

    Added employee model and animations (rough texture and model)

---
+++ UnityBossManGame/Assets/Scripts/employeeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class employeeBehavior : MonoBehaviour {
+
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Called from the editor from the cog icon, useful for hooking up dependant scene attributes
+    void Reset()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Debug.Log("Resetting animator = "+ animator);
+        Debug.Assert(animator != null,"Can't find an animator attached to object");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TriggerNextEmployee()
+    {
+
+    }
+}
